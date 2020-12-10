d86145eb08a08c30dd691a78d6060efc3ce53fcf
Vanessa Boehm <boehmvanessa@gmail.com>
2020-12-10 14:59:27 -0800

    reading from original files

--- sdss_pae/sdss_dataset/SDSS.py
+++ sdss_pae/sdss_dataset/SDSS.py
@@ -2,18 +2,14 @@
 
 import tensorflow_datasets as tfds
 from astropy.io import fits
+import json
 import tensorflow as tf
 import os
-import numpy as np
-            #f.close()
-            #f.close()
 
 # TODO(SDSS): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
-Description is **formatted** as markdown.
+selected features from SDSS dataset in 
 
-It should also contain any processing which has been applied (if any),
-(e.g. corrupted example skipped, images cropped,...):
 """
 
 # TODO(SDSS): BibTeX citation
@@ -37,7 +33,7 @@ class Sdss(tfds.core.GeneratorBasedBuilder):
         description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
-            'image': tfds.features.Tensor(shape=(4000,1),dtype=tf.float32),
+            'flux': tfds.features.Tensor(shape=(None,1),dtype=tf.float32),
         #    'label': tfds.features.ClassLabel(names=['STAR', 'QSR', 'GALAXY']),
         }),
         # If there's a common (input, target) tuple from the
@@ -56,21 +52,26 @@ class Sdss(tfds.core.GeneratorBasedBuilder):
             name=tfds.Split.TRAIN,
             # These kwargs will be passed to _generate_examples
             gen_kwargs={
-            "data_dir": '/global/cscratch1/sd/vboehm/Datasets/SDSS_BOSS_data/',
+            "local_dir": '/global/cscratch1/sd/vboehm/Datasets/SDSS_BOSS_data/',
+            "data_dir": '/global/project/projectdirs/cosmo/data/sdss/dr16/sdss/spectro/redux/v5_13_0/'
             },
         ),
     ]
 
-  def _generate_examples(self, data_dir):
+  def _generate_examples(self, local_dir, data_dir):
     """Yields examples."""
-    print('begin')
-    # Read the maps from the directory
-    for ii, image_file in enumerate(tf.io.gfile.listdir(data_dir)):
-        print(os.path.join(data_dir ,image_file))
-        with tf.io.gfile.GFile(os.path.join(data_dir ,image_file), mode='rb') as f:
-            data = 1*fits.getdata(f, 0).astype('float32')
-            #for ii in range(1000):
-            im = tf.expand_dims(data[0][0:4000],-1)
-            print(im)
+    with open(os.path.join(local_dir,'datafiles.txt'), 'r') as infile:
+        for line in infile:
+            filenames = json.loads(line)
+    infile.close()
+
+    for jj, data_file in enumerate(filenames[0:10]):
+        print(data_file)
+        with tf.io.gfile.GFile(os.path.join(data_dir ,data_file), mode='rb') as f:
+            #flux = 1*fits.getdata(f, 0).astype('float32')
+            hdulist = fits.open(f)
+            flux    = hdulist[0].data.astype('float32')
+            for ii in range(1000):
+                spec = tf.expand_dims(flux[ii],-1)
+                yield '%d'%int(jj*1000+ii), {'flux': spec.numpy()}#, "label":label}
             f.close()
-            yield '%d'%int(ii), {'image': im.numpy()}#, "label":label}
