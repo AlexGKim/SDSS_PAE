9cf0882f44872fabcd56ca4344c7744b6cfde14f
Vanessa Boehm <boehmvanessa@gmail.com>
2020-12-10 17:18:25 -0800

    fully working tf dataset production pipeline

--- sdss_pae/sdss_dataset/SDSS.py
+++ sdss_pae/sdss_dataset/SDSS.py
@@ -5,11 +5,17 @@ from astropy.io import fits
 import json
 import tensorflow as tf
 import os
+import numpy as np
 
 # TODO(SDSS): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
-selected features from SDSS dataset in 
-
+selected features from spAll and spZbest files 
+'flux': measured spectrum in  
+'inv_var': inverse variance
+'and_mask': and mask (set to 1 for all non-zero entries)
+'coeffs': c0, c1, npix. calculate wavelengths with `10.**(c0 + c1 * np.arange(npix))`
+'label': type of object, 'STAR'==0, 'QSO'==1, 'GALAXY'==2
+'redshift': object redshift estimate
 """
 
 # TODO(SDSS): BibTeX citation
@@ -27,20 +33,24 @@ class Sdss(tfds.core.GeneratorBasedBuilder):
 
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
-    # TODO(SDSS): Specifies the tfds.core.DatasetInfo object
     return tfds.core.DatasetInfo(
         builder=self,
         description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
             'flux': tfds.features.Tensor(shape=(None,1),dtype=tf.float32),
-        #    'label': tfds.features.ClassLabel(names=['STAR', 'QSR', 'GALAXY']),
+            'inv_var': tfds.features.Tensor(shape=(None,1),dtype=tf.float32),
+            'and_mask': tfds.features.Tensor(shape=(None,1),dtype=tf.int32),
+            'coeffs': tfds.features.Tensor(shape=(3,1), dtype=tf.float32),
+            'label': tfds.features.ClassLabel(names=['STAR', 'QSO', 'GALAXY']),
+            'redshift': tfds.features.Tensor(shape=(),dtype=tf.float32),
+            'filename': tfds.features.Text(),
         }),
         # If there's a common (input, target) tuple from the
         # features, specify them here. They'll be used if
         # `as_supervised=True` in `builder.as_dataset`.
         supervised_keys=None,#('flux'),  # Set to `None` to disable
-        homepage='https://dataset-homepage/',
+        homepage='https://www.sdss.org/science/data-release-publications/',
         citation=_CITATION,
     )
 
@@ -65,13 +75,39 @@ class Sdss(tfds.core.GeneratorBasedBuilder):
             filenames = json.loads(line)
     infile.close()
 
-    for jj, data_file in enumerate(filenames[0:10]):
-        print(data_file)
+    with open(os.path.join(local_dir,'z_files.txt'), 'r') as infile:
+        for line in infile:
+            z_files = json.loads(line)
+    infile.close()
+
+    for jj, data_file in enumerate(filenames[0:100]):
+        with tf.io.gfile.GFile(os.path.join(data_dir ,z_files[jj]), mode='rb') as f:
+            print(data_file)            
+            hdulist   = fits.open(f)
+            zstruc    = hdulist[1].data
+            redshifts = zstruc['z'].astype('float32')
+            classes   = zstruc['class']
+            f.close()
+
         with tf.io.gfile.GFile(os.path.join(data_dir ,data_file), mode='rb') as f:
-            #flux = 1*fits.getdata(f, 0).astype('float32')
+            
             hdulist = fits.open(f)
             flux    = hdulist[0].data.astype('float32')
+            ivar_   = hdulist[1].data.astype('float32')
+            amask_  = (hdulist[2].data==0).astype('int32')
+            
+            c0      = hdulist[0].header['coeff0']
+            c1      = hdulist[0].header['coeff1']
+            npix    = hdulist[0].header['naxis1']
+            
+            coeffs  = np.expand_dims(np.asarray((c0,c1,npix)),-1).astype('float32') 
+            
             for ii in range(1000):
-                spec = tf.expand_dims(flux[ii],-1)
-                yield '%d'%int(jj*1000+ii), {'flux': spec.numpy()}#, "label":label}
+                spec     = np.expand_dims(flux[ii],-1)
+                ivar     = np.expand_dims(ivar_[ii],-1)
+                amask    = np.expand_dims(amask_[ii],-1)
+                redshift = redshifts[ii]
+                CLASS    = classes[ii]
+                                
+                yield '%d'%int(jj*1000+ii), {'filename': data_file, 'flux': spec, 'inv_var': ivar, 'and_mask': amask, 'coeffs': coeffs, 'redshift': redshift, 'label':CLASS}
             f.close()
